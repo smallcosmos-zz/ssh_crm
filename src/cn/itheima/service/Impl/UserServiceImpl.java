176b9600176fe8b20514ccda7fed3477cabcf5dd
zhaozhaoli256818 <1191599851@qq.com>
2020-02-21 15:31:44 +0800

    对用户密码进行MD5加密

--- src/cn/itheima/service/Impl/UserServiceImpl.java
+++ src/cn/itheima/service/Impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package cn.itheima.service.Impl;
 import cn.itheima.dao.UserDao;
 import cn.itheima.domain.User;
 import cn.itheima.service.UserService;
+import cn.itheima.utils.MD5Utils;
 
 import java.util.List;
 
@@ -22,7 +23,7 @@ public class UserServiceImpl implements UserService {
         if(exitU == null){
             throw new RuntimeException("用户名错误！");
         }
-        if(!exitU.getUser_password().equals(u.getUser_password())){
+        if(!exitU.getUser_password().equals(MD5Utils.md5(u.getUser_password()))){
             throw new RuntimeException("密码错误！");
         }
         //判断密码是否正确
@@ -37,6 +38,8 @@ public class UserServiceImpl implements UserService {
             //用户已经存在，抛出异常
             throw new RuntimeException("该用户已存在");
         }
+        //对用户密码进行MD5加密
+        user.setUser_password(MD5Utils.md5(user.getUser_password()));
         //2 用户不存在执行保存
         userDao.save(user);
     }
